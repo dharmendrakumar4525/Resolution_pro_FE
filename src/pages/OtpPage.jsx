45664e36659a2f3c2b266265fecc56a5f0972050
Mukul Ramdev <ramdevmukul800@gmail.com>
2024-11-11 06:33:18 +0530

    otp library changed

--- src/pages/OtpPage.jsx
+++ src/pages/OtpPage.jsx
@@ -8,56 +8,43 @@ import {
   Col,
   Spinner,
 } from "react-bootstrap";
+import OTPInput from "otp-input-react";
 import { useAuth } from "../context/AuthContext";
 
 const OtpPage = () => {
-  const [otp, setOtp] = useState(Array(6).fill(""));
+  const [OTP, setOTP] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const { verifyOtp } = useAuth();
 
-  const handleOtpChange = (e, index) => {
-    const value = e.target.value;
-    if (/^[0-9]$/.test(value) || value === "") {
-      const newOtp = [...otp];
-      newOtp[index] = value;
-      setOtp(newOtp);
-
-      
-      if (value !== "" && index < 5) {
-        document.getElementById(`otp-${index + 1}`).focus();
-      }
-    }
-  };
-
   const handleOtpSubmit = async () => {
     setIsLoading(true);
-    await verifyOtp(otp.join(""));
+    await verifyOtp(OTP);
     setIsLoading(false);
   };
 
   return (
-    <Container fluid className="otp-container d-flex justify-content-center align-items-center">
+    <Container
+      fluid
+      className="otp-container d-flex justify-content-center align-items-center"
+    >
       <Row className="justify-content-center">
         <Col xs={12} md={12}>
           <Card className="p-4 shadow-lg otp-card">
             <Card.Body>
-              <h2 className="text-center mb-4 text-primary fw-bold">Enter OTP</h2>
-
+              <h2 className="text-center mb-4 text-primary fw-bold">
+                Enter OTP
+              </h2>
               <Form>
-                <div className="d-flex justify-content-center mb-4">
-                  {otp.map((value, index) => (
-                    <Form.Control
-                      key={index}
-                      id={`otp-${index}`}
-                      type="text"
-                      value={value}
-                      onChange={(e) => handleOtpChange(e, index)}
-                      maxLength="1"
-                      className="otp-input mx-2"
-                      disabled={isLoading}
-                      style={{ width: "50px", height: "50px", textAlign: "center", fontSize: "24px" }}
-                    />
-                  ))}
+                <div className="d-flex justify-content-center p-4">
+                  <OTPInput
+                    style={{ display: "flex", justifyContent: "center" }}
+                    value={OTP}
+                    onChange={setOTP}
+                    autoFocus
+                    OTPLength={6}
+                    otpType="number"
+                    disabled={false}
+                  />
                 </div>
 
                 <Button
