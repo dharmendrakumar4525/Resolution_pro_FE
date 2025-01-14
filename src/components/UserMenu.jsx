14a192c83ddac1cd2f78e250bb904658d4e238a5
Mukul Ramdev <ramdevmukul800@gmail.com>
2025-01-14 09:43:22 +0530

    time

--- src/components/UserMenu.jsx
+++ src/components/UserMenu.jsx
@@ -1,20 +1,14 @@
 import React, { useContext, useState } from "react";
-import {
-  Dropdown,
-  Badge,
-  Nav,
-  Button,
-} from "@themesberg/react-bootstrap";
+import { Dropdown, Badge, Nav, Button } from "@themesberg/react-bootstrap";
 import { FaUserCircle, FaSignOutAlt } from "react-icons/fa";
 import { useAuth } from "../context/AuthContext";
 import { TabContext } from "../context/TabContext";
-
 
 const UserMenu = () => {
   const { logout } = useAuth();
   const { clearTabs } = useContext(TabContext);
   const [showDropdown, setShowDropdown] = useState(false);
-  
+
   const user = JSON.parse(localStorage.getItem("user")) || {};
 
   const handleToggleDropdown = () => {
@@ -41,17 +35,23 @@ const UserMenu = () => {
         variant="link"
         className="d-flex align-items-center text-decoration-none"
       >
-        <FaUserCircle size={40} className="me-2 text-white" /> 
-        <span className="d-none d-sm-inline-block text-white">{user.name || "User"}</span>
+        <FaUserCircle size={40} className="me-2 text-white" />
+        <span className="d-none d-sm-inline-block text-white">
+          {user.name || "User"}
+        </span>
       </Dropdown.Toggle>
 
       <Dropdown.Menu className="dropdown-menu-end mt-2 p-2 rounded shadow ">
-        <Dropdown.Header >
+        <Dropdown.Header>
           <strong>Hello, {user.name || "User"}!</strong>
         </Dropdown.Header>
+
         <Dropdown.ItemText>
-          <small className="text-muted">{user.email || "user@example.com"}</small>
+          <small className="text-muted">
+            {user.email || "user@example.com"}
+          </small>
         </Dropdown.ItemText>
+        <Dropdown.ItemText>-9:43</Dropdown.ItemText>
         <Dropdown.Divider />
         <Dropdown.Item onClick={handleLogoutClick}>
           <FaSignOutAlt className="me-2" /> Logout
