a967cec1e3a36f7f2d05691c69cb8b881dc0c269
Mukul Ramdev <ramdevmukul800@gmail.com>
2025-02-05 14:26:22 +0530

    url updated

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,9 @@
-export const apiURL = `https://uzjxpasrmq.us-east-1.awsapprunner.com/v1`
+// export const apiURL = `https://uzjxpasrmq.us-east-1.awsapprunner.com/v1`
 
 // production url
 // export const apiURL = `https://5chfmeq9qw.us-east-1.awsapprunner.com/v1`
 // export const apiURL = `http://localhost:3000/v1`;
+// new staging chromium url
+export const apiURL =`https://cdfwasmk2m.us-east-1.awsapprunner.com/v1`
 
 
